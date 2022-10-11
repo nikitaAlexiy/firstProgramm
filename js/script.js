7890176a66c82ff392a191afe1e679c5368d3d7a
Nikita <nikitaaleksij1@gmail.com>
2022-10-11 19:19:41 +0300

    made some functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
-let personalMovieDB = {
+let numberOfFilms; 
+function start(){
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
+
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -7,19 +17,23 @@ let personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; ){
+function rememberMyFilms() {
+for (let i = 0; i < 2; i++){
     const a = prompt('Какой ваш последний просмотренный фильм?', ''),   
           b = prompt('На сколько оцените его?', '');
 
     if(a != null && b != null && a != '' && b != '' && a.length <= 50) {
         personalMovieDB.movies[a] = b;
-        i++; 
     } else {
-        alert('Введите название фильма не длинее чем 50 символов'); continue;
+        alert('Введите название фильма не длинее чем 50 символов'); 
+        i--;
     }
+}    
 }
 
+rememberMyFilms();
 
+function detectPersonalLevel(){
 if(personalMovieDB.count < 10 ){
     alert('Вы просто любитель');
 }else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
@@ -28,8 +42,28 @@ if(personalMovieDB.count < 10 ){
     alert('Вы киноман');
 }else{
     alert('Ошибочка');
+}  
 }
-console.log(personalMovieDB);
+
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden){
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for(let i = 1; i <= 3; i++){
+        personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
+
+
 
 
 
